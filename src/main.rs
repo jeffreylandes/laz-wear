1db18c78dabc31df7009677029e73cb7559c1aa9
jeffreylandes <jeffrey@spacemaker.ai>
2022-08-09 22:03:50 -0400

    feat: add help command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
 use std::env;
+use colored::Colorize;
 
 mod parser;
 
+fn get_file_index(args: &Vec<String>) -> usize {
+    let position = args.iter().position(|x| x == "--file");
+    let file_index = match position {
+        Some(index) => index,
+        None => {
+            eprintln!("{}: Incorrect usage.", "error".red());
+            eprintln!("Correct usage: {} --file file.las", "laswear".cyan());
+            panic!("No --file argument found.");
+        }
+    };
+    if file_index == args.len() - 1 {
+        panic!("No file specified.");
+    }
+    file_index + 1
+}
+
+fn asking_for_help(args: &Vec<String>) -> bool {
+    let help_exists = args.iter().any(|x| x == "--help");
+    help_exists
+}
+
+fn print_help() {
+    println!("Expected usage:\n");
+    println!("   {} --file <file> --info <info\n", "laswear".cyan());
+    println!("\t {} - The file to be processed. Only .las files are currently supported.", "--file".magenta());
+    println!("\t {} - {}, The information that is parsed from the file. Options are:", "--info".magenta(), "Optional".bold());
+    println!("\t\t - {} : Stastics describing the distribution of the classified points.", "classification".bold());
+    println!("\t\t - {} : Statistics describing the distribution of the ground point.", "ground".bold());
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Expected usage: laz-wear file.las");
+
+    if asking_for_help(&args) {
+        print_help();
         return;
     }
-    let file_name = &args[1];
+
+    let file_index = get_file_index(&args);
+    let file_name = &args[file_index];
     let las_file: parser::LasFile = match parser::read_file(file_name) {
         Ok(parsed_file) => parsed_file,
         Err(e) => panic!("Encountered error {}", e),
